95185038bbbb90876e98c80c2af74c332baa472a
Luis A. Rodriguez <larc63@gmail.com>
2025-11-20 17:18:22 -0600

    Added drag, still WIP

--- react-playground/solid-todo/src/TODOList.tsx
+++ react-playground/solid-todo/src/TODOList.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import {useRef} from "react";
 import ListItem from "./ListItem";
 
 function TODOList() {
-    return (<div>
-        <ListItem/>
+    const handleItemMove = (rank: number, x: number, y: number) => {
+        console.log(`item ${rank} moved to x/y ${x}, ${y}`);
+    }
+    const listRef = useRef(null);
+    return (<div ref={listRef}>
+        <ListItem onItemMove={handleItemMove} rank={0} />
     </div>)
 }
 
