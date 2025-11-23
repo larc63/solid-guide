3430dbf4e7c0d3f29f2f60567092dd773520f009
Luis A. Rodriguez <larc63@gmail.com>
2025-11-23 01:08:37 -0600

    Added cors Implemented list getter

--- solid-todo/api/app.js
+++ solid-todo/api/app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
 const db = require('./lib/db');
-
-
 
 const indexRouter = require('./routes/index');
 const notesRouter = require('./routes/notes');
@@ -22,12 +22,13 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', notesRouter);
+app.use('/notes', notesRouter);
 app.use('/initialize', initRouter);
 
 // catch 404 and forward to error handler
--- solid-todo/api/routes/notes.js
+++ solid-todo/api/routes/notes.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../lib/db');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a list of notes');
+class TODOListController {
+  /**
+   * Description
+   * @param {any} userId
+   * @returns {any}
+   */
+  static async getNotes(userId) {
+    try {
+      // TODO: change to find, to later iterate on all notes for a given user
+      const list = await db.models.todolists.findOne({
+        where: { list_id: 1 }
+      });
+      console.log(`Found list ${list}`);
+
+      const items = await db.models.listitems.findAll({
+        where: { list_id: 1 }
+      });
+      console.log(`found ${items.length} items`);
+      list.setDataValue('items', items);
+      return list;
+    } catch (error) {
+      return null;
+    }
+  }
+}
+
+/* GET notes for user listing. */
+router.get('/', async function (req, res, next) {
+  try {
+    const list = await TODOListController.getNotes(1);
+    res.json(list);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
