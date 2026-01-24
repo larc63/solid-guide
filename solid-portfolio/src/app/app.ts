698955731a2c948495a42e044f32d85dd8ad6211
Luis A. Rodriguez <larc63@gmail.com>
2026-01-24 07:33:59 -0600

    Refactor app structure and add content component with styles; integrate new font assets

--- solid-portfolio/src/app/app.ts
+++ solid-portfolio/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Content } from "./content/content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Content, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
