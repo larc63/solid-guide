3106a61d2a0f99bc94bf8fc5a3d9e0b566862ead
Luis A. Rodriguez <larc63@gmail.com>
2026-01-24 19:44:09 -0600

    Added gridview component

--- solid-portfolio/src/app/app.ts
+++ solid-portfolio/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Content } from "./content/content";
+import { GridView } from './grid-view/grid-view';
 
 @Component({
   selector: 'app-root',
-  imports: [Content, RouterOutlet],
+  imports: [Content, GridView, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
