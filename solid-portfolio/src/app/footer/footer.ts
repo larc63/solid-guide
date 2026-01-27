68b91d0e75f0093bfb444f9f3c5f4af916f37751
Luis A. Rodriguez <larc63@gmail.com>
2026-01-26 21:56:44 -0600

    Added tooltips and description for resume pop-out

--- solid-portfolio/src/app/footer/footer.ts
+++ solid-portfolio/src/app/footer/footer.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
+import { faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-footer',
@@ -9,6 +9,5 @@ import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
     styleUrl: './footer.scss',
 })
 export class Footer {
-faIcon = faIdBadge;
-
+    faIcon = faSquareArrowUpRight;
 }
